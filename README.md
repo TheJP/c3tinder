6157d704d6944cd0cd7f4b28dd0b9d7ee52dd1bc
TheJP <jp_55555@hotmail.com>
2024-12-27 22:00:10 +0100

    Worked on website
    Made it somewhat acceptably looking.

---
+++ README.md
@@ -0,0 +1,7 @@
+# C3Tinder
+
+Please don't hack or DDoS. Feel free to let me know if you find any issues, bugs, ...
+
+## Credits
+
+Icons from <https://iconoir.com/>
--- website/index.js
+++ website/index.js
@@ -2,10 +2,10 @@
 
 let data = {};
 let filtered_shifts = {};
+let current_shift = {};
 
 const swiper = new Swiper('#swiper', {
     initialSlide: 1,
-    init: false,
 });
 
 const request = new XMLHttpRequest();
@@ -30,21 +30,32 @@ request.addEventListener("readystatechange", function () {
         const remember = document.getElementById("remember");
         remember.addEventListener("click", () => swiper.slideNext(300, true));
 
-        const keys = Object.keys(filtered_shifts);
-        show_card(filtered_shifts[keys[0]]);
+        show_random_card();
     } else {
         console.log("failed", this.responseText);
     }
 });
 request.send();
 
+function show_random_card() {
+    const keys = Object.keys(filtered_shifts);
+    const selected = keys[Math.floor(Math.random() * keys.length)];
+    show_card(filtered_shifts[selected]);
+    delete filtered_shifts[selected];
+}
+
 function show_card(shift) {
+    current_shift = shift;
+
     const header = document.getElementById("shift_header");
     header.textContent = shift["name"];
     header.setAttribute("href", shift["link"]);
 
+    const time = document.getElementById("shift_time");
+    time.textContent = `${shift["start"]} - ${shift["end"]}`;
+
     const date = document.getElementById("shift_date");
-    date.textContent = `${shift["day"]} ${shift["start"]} - ${shift["end"]}`;
+    date.textContent = `${shift["day"]}`;
 
     const location = document.getElementById("shift_location");
     location.textContent = shift["location"]["name"];
@@ -53,6 +64,22 @@ function show_card(shift) {
     const angel_type = document.getElementById("shift_angel_type");
     angel_type.textContent = shift["angel"]["name"];
     angel_type.setAttribute("href", shift["angel"]["link"]);
-
-    swiper.init();
 }
+
+swiper.on("slideChangeTransitionEnd", () => {
+    if (swiper.activeIndex == 0) {
+        // Reject
+    } else if (swiper.activeIndex == 2) {
+        // Accept
+    }
+
+    if (swiper.activeIndex != 1) {
+        show_random_card();
+
+        swiper.allowTouchMove = false;
+        setTimeout(() => {
+            swiper.slideTo(1, 200, false);
+            swiper.allowTouchMove = true;
+        }, 300);
+    }
+})
