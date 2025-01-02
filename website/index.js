5206eba6aa87f8532ce2ba6bbfb8712f1f1c172b
TheJP <jp_55555@hotmail.com>
2025-01-02 14:53:02 +0100

    Worked on shift filters

--- website/index.js
+++ website/index.js
@@ -3,6 +3,8 @@
 let data = {};
 let filtered_shifts = {};
 let current_shift = {};
+let location_filters = {};
+let angel_filters = {};
 
 const REJECTS_KEY = "rejects";
 const REMEMBERED_KEY = "remembered";
@@ -14,30 +16,37 @@ const swiper = new Swiper('#swiper', {
     initialSlide: 1,
 });
 
-window.addEventListener('DOMContentLoaded', function () {
-    const cart = this.document.getElementById("cart");
-    const close_cart = document.getElementById("close_cart");
-    close_cart.addEventListener("click", () => {
-        const hide_animation = cart.animate([
-            { transform: "translateY(0)" },
+window.addEventListener('DOMContentLoaded', () => {
+    const close_buttons = document.getElementsByClassName("close-button");
+    for (let close_button of close_buttons) {
+        close_button.addEventListener("click", function () {
+            const modal = this.parentNode;
+            const hide_animation = modal.animate([
+                { transform: "translateY(0)" },
+                { transform: "translateY(100%)" },
+            ], {
+                duration: 200
+            });
+            hide_animation.onfinish = () => {
+                modal.style["display"] = "none";
+            }
+        });
+    }
+
+    const open_modal = (modal) => {
+        modal.style["display"] = "grid";
+        modal.animate([
             { transform: "translateY(100%)" },
+            { transform: "translateY(0)" },
         ], {
             duration: 200
         });
-        hide_animation.onfinish = () => {
-            cart.style["display"] = "none";
-        }
-    });
+    };
 
     const open_cart = document.getElementById("open_cart");
     open_cart.addEventListener("click", () => {
-        cart.style["display"] = "grid";
-        cart.animate([
-            { transform: "translateY(100%)" },
-            { transform: "translateY(0)" },
-        ], {
-            duration: 200
-        });
+        const cart = this.document.getElementById("cart");
+        open_modal(cart);
 
         const cart_content = document.getElementById("cart_content");
         cart_content.innerHTML = "";
@@ -86,6 +95,76 @@ window.addEventListener('DOMContentLoaded', function () {
             angel_type.setAttribute("target", "_blank");
         }
     });
+
+    // Filters
+    let disable_checkbox_events = false;
+    const locations = document.getElementById("settings_locations");
+    const angels = document.getElementById("settings_angels");
+
+    const open_settings = document.getElementById("open_settings");
+    open_settings.addEventListener("click", () => {
+        const settings = document.getElementById("settings");
+        open_modal(settings);
+
+        const add_checkboxes = (div, types, filters) => {
+            div.innerHTML = "";
+            const keys = Object.keys(types);
+            keys.sort((a, b) => types[a].localeCompare(types[b]));
+            for (let key of keys) {
+                if (!Object.hasOwn(filters, key)) {
+                    filters[key] = true;
+                }
+                const label = document.createElement("label");
+                div.append(label);
+
+                const checkbox = document.createElement("input");
+                label.append(checkbox);
+                checkbox.setAttribute("id", `${div.getAttribute("id")}-${key}`);
+                checkbox.setAttribute("type", "checkbox");
+                checkbox.checked = filters[key];
+                checkbox.addEventListener("change", () => {
+                    if (disable_checkbox_events) {
+                        return;
+                    }
+                    filters[key] = checkbox.checked;
+                    create_filtered_shifts();
+                    show_random_card();
+                });
+
+                const text = document.createTextNode(types[key]);
+                label.append(text);
+            }
+        };
+
+        add_checkboxes(locations, data["locations"], location_filters);
+        add_checkboxes(angels, data["angel_types"], angel_filters);
+    });
+    setTimeout(() => open_settings.click(), 200); // TODO: Remove
+
+    const set_all_checkboxes = (div, value, filters) => {
+        disable_checkbox_events = true;
+        for (let checkbox of div.getElementsByTagName("input")) {
+            checkbox.checked = value;
+        }
+        disable_checkbox_events = false;
+
+        for (let key of Object.keys(filters)) {
+            filters[key] = value;
+        }
+
+        create_filtered_shifts();
+        show_random_card();
+    };
+
+    const locations_all = document.getElementById("locations_all");
+    locations_all.addEventListener("click", () => set_all_checkboxes(locations, true, location_filters));
+    const locations_none = document.getElementById("locations_none");
+    locations_none.addEventListener("click", () => set_all_checkboxes(locations, false, location_filters));
+
+    const angel_all = document.getElementById("angel_all");
+    angel_all.addEventListener("click", () => set_all_checkboxes(angels, true, angel_filters));
+    const angel_none = document.getElementById("angel_none");
+    angel_none.addEventListener("click", () => set_all_checkboxes(angels, false, angel_filters));
 });
 
 const request = new XMLHttpRequest();
@@ -128,6 +207,12 @@ function store_remembered() {
 
 function show_random_card() {
     const keys = Object.keys(filtered_shifts);
+    if (keys.length === 0) {
+        // TODO: Show message to user.
+        console.log("there are no shifts remaining for the selected filters");
+        return;
+    }
+
     const selected = keys[Math.floor(Math.random() * keys.length)];
     filtered_shifts[selected]["id"] = selected;
     show_card(filtered_shifts[selected]);
@@ -172,10 +257,44 @@ function update_and_bounce_bubble() {
 }
 
 function create_filtered_shifts() {
-    filtered_shifts = data["shifts"];
+    filtered_shifts = { ...data["shifts"] };
     for (let reject of Object.keys(rejects)) {
         delete filtered_shifts[reject];
     }
+
+    const delete_keys = [];
+
+    for (let location of Object.keys(location_filters)) {
+        if (location_filters[location]) {
+            continue;
+        }
+
+        for (let shift_key of Object.keys(filtered_shifts)) {
+            if (filtered_shifts[shift_key]["location"]["id"] === location) {
+                delete_keys.push(shift_key);
+            }
+        }
+    }
+
+    for (let angel of Object.keys(angel_filters)) {
+        if (angel_filters[angel]) {
+            continue;
+        }
+
+        for (let shift_key of Object.keys(filtered_shifts)) {
+            if (filtered_shifts[shift_key]["angel"]["id"] === angel) {
+                delete_keys.push(shift_key);
+            }
+        }
+    }
+
+    for (let shift_key of delete_keys) {
+        delete filtered_shifts[shift_key];
+    }
+
+    // const count = Object.keys(filtered_shifts).length;
+    const count = document.getElementById("filtered_count");
+    count.innerText = Object.keys(filtered_shifts).length.toLocaleString();
 }
 
 function remove_shift(id) {
@@ -187,16 +306,16 @@ function remove_shift(id) {
 }
 
 swiper.on("slideChangeTransitionEnd", () => {
-    if (swiper.activeIndex == 0) {
+    if (swiper.activeIndex === 0) {
         rejects[current_shift["id"]] = true;
         store_rejects();
-    } else if (swiper.activeIndex == 2) {
+    } else if (swiper.activeIndex === 2) {
         remembered[current_shift["id"]] = current_shift;
         store_remembered();
         update_and_bounce_bubble();
     }
 
-    if (swiper.activeIndex != 1) {
+    if (swiper.activeIndex !== 1) {
         show_random_card();
 
         swiper.allowTouchMove = false;
